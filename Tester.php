0e1f90b169fabeca14ce5b06854446b15491a69a
Kelvins Aghayedo <kelvinseghosa@gmail.com>
2023-06-27 18:25:10 +0100

    Updated IV params
    Before this update, IV was a static value in the AES_CLASS.php but now it is passed as a parameter to the class. Variable now available on the Tester.php file, just replace the value.

--- Tester.php
+++ Tester.php
@@ -1,7 +1,7 @@
 <?php  
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 $key="replaceWithYourKEY";
 $iv="replaceWithYourIV";
@@ -9,7 +9,7 @@ $iv="replaceWithYourIV";
 include_once('AES_CLASS.php');
 
 $data['PaymentReference']="2300104003420992";
-$data['assessmentId']= "FA-1687282905";
+$data['assessmentId']= "1687282905";
 $data['Amount'] = "200";
 $data['BankCode']= "33";
 $data['Ref'] = "221122";
@@ -21,7 +21,7 @@ $inputData=$data;
 $inputText = $data;
 $inputKey = $key;
 $blockSize = 128;
-$aes = new AES($inputText, $inputKey, $blockSize);
+$aes = new AES($inputText, $inputKey, $iv, $blockSize);
 $enc = $aes->encrypt();
 $enc=bin2hex(base64_decode($enc));
 
@@ -29,10 +29,9 @@ echo "Encrypted: ".$enc;
 
 
 //Now lets decrypt the encoded;
-
+ 
 $cyph=base64_encode(hex2bin($enc));
 $aes->setData($cyph);
 $dec=$aes->decrypt(); 
-echo  "Decrypted: ".$dec; 
-
+echo  "<br><br>Decrypted: ".$dec;  
 ?> 
